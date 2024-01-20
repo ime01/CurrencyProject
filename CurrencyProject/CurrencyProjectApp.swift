651a98222a46071d07ae283c470919400ee64fe7
Imevbore <ketuimevbore@yahoo.com>
2024-01-20 22:04:09 +0000

    Initial Commit

---
+++ CurrencyProject/CurrencyProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyProjectApp.swift
+//  CurrencyProject
+//
+//  Created by Elena for AP on 20/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
